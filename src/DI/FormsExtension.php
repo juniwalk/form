b5bd3d9bc464ccdbd7d55566e41abd41a3364b21
Martin Procházka <juniwalk@outlook.cz>
2015-10-22 11:11:16 +0200

    ADD: DI Extension for FormLocator

---
+++ src/DI/FormsExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   Forms
+ * @link      https://github.com/juniwalk/forms
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Forms\DI;
+
+final class FormsExtension extends \Nette\DI\CompilerExtension
+{
+    /**
+     * Register extension into DI container.
+     */
+    public function loadConfiguration()
+    {
+        // Register all provided forms into DI container
+        foreach ($this->getConfig() as $name => $interface) {
+            $this->getContainerBuilder()->addDefinition($this->prefix($name))
+                ->setImplement($interface);
+        }
+
+        // Register factory class into DI container
+        $this->getContainerBuilder()->addDefinition($this->prefix('locator'))
+            ->setClass('JuniWalk\Forms\FormLocator', [$this->name]);
+    }
+}
