5648ece39e583c840b9efd3c160015e6e05fa12a
Martin Procházka <juniwalk@outlook.cz>
2015-12-03 10:05:46 +0100

    IMP: Switch to tabs for indentation

--- src/DI/FormsExtension.php
+++ src/DI/FormsExtension.php
@@ -12,19 +12,19 @@ namespace JuniWalk\Forms\DI;
 
 final class FormsExtension extends \Nette\DI\CompilerExtension
 {
-    /**
-     * Register extension into DI container.
-     */
-    public function loadConfiguration()
-    {
-        // Register all provided forms into DI container
-        foreach ($this->getConfig() as $name => $interface) {
-            $this->getContainerBuilder()->addDefinition($this->prefix($name))
-                ->setImplement($interface);
-        }
+	/**
+	 * Register extension into DI container.
+	 */
+	public function loadConfiguration()
+	{
+		// Register all provided forms into DI container
+		foreach ($this->getConfig() as $name => $interface) {
+			$this->getContainerBuilder()->addDefinition($this->prefix($name))
+				->setImplement($interface);
+		}
 
-        // Register factory class into DI container
-        $this->getContainerBuilder()->addDefinition($this->prefix('locator'))
-            ->setClass('JuniWalk\Forms\FormLocator', [$this->name]);
-    }
+		// Register factory class into DI container
+		$this->getContainerBuilder()->addDefinition($this->prefix('locator'))
+			->setClass('JuniWalk\Forms\FormLocator', [$this->name]);
+	}
 }
--- src/Form.php
+++ src/Form.php
@@ -15,84 +15,84 @@ use Nette\Localization\ITranslator;
 
 class Form extends \Nette\Application\UI\Form
 {
-    /**
-     * Collect dependencies of the Form component.
-     * @param IContainer  $parent  Parent container
-     * @param string      $name    Component name
-     */
-    public function __construct(IContainer $parent, $name)
-    {
-        // Call parent constructor with params
-        parent::__construct($parent, $name);
+	/**
+	 * Collect dependencies of the Form component.
+	 * @param IContainer  $parent  Parent container
+	 * @param string      $name    Component name
+	 */
+	public function __construct(IContainer $parent, $name)
+	{
+		// Call parent constructor with params
+		parent::__construct($parent, $name);
 
-        // Add internal success handler into event listeners
-        $this->onSuccess[] = function($form, $data) {
-            $this->handleSuccess($form, $data);
-        };
-    }
-
-
-    /**
-     * Adds global error message.
-     * @param string  $message  Error message
-     */
-    public function addError($message, array $params = [])
-    {
-        // If there is translator defined in the Form
-        if ($this->translator instanceof ITranslator) {
-            $message = $this->translator->translate($message, $params);
-        }
-
-        return parent::addError($message);
-    }
+		// Add internal success handler into event listeners
+		$this->onSuccess[] = function($form, $data) {
+			$this->handleSuccess($form, $data);
+		};
+	}
 
 
-    /**
-     * Disable whole Form component.
-     * @param  bool  $value  Disabled value
-     * @return static
-     */
-    public function setDisabled($value)
-    {
-        foreach ($this->getComponents() as $control) {
-            $control->setDisabled($value);
-        }
+	/**
+	 * Adds global error message.
+	 * @param string  $message  Error message
+	 */
+	public function addError($message, array $params = [])
+	{
+		// If there is translator defined in the Form
+		if ($this->translator instanceof ITranslator) {
+			$message = $this->translator->translate($message, $params);
+		}
 
-        return $this;
-    }
-
-
-    /**
-     * Set attribute to the form element.
-     * @param  string  $key    Attribute name
-     * @param  mixed   $value  New value
-     * @return static
-     */
-    public function setAttribute($key, $value)
-    {
-        $this->getElementPrototype()->setAttribute($key, $value);
-        return $this;
-    }
+		return parent::addError($message);
+	}
 
 
-    /**
-     * Get value of the form element attribute.
-     * @param  string  $key  Attribute name
-     * @return mixed
-     */
-    public function getAttribute($key)
-    {
-        return $this->getElementPrototype()->getAttribute($key);
-    }
+	/**
+	 * Disable whole Form component.
+	 * @param  bool  $value  Disabled value
+	 * @return static
+	 */
+	public function setDisabled($value)
+	{
+		foreach ($this->getComponents() as $control) {
+			$control->setDisabled($value);
+		}
+
+		return $this;
+	}
 
 
-    /**
-     * Signal - On successfull submit.
-     * @param  static  $form  Form instance
-     * @param  mixed   $data  Submited data
-     * @return null
-     */
-    protected function handleSuccess($form, $data)
-    {
-    }
+	/**
+	 * Set attribute to the form element.
+	 * @param  string  $key    Attribute name
+	 * @param  mixed   $value  New value
+	 * @return static
+	 */
+	public function setAttribute($key, $value)
+	{
+		$this->getElementPrototype()->setAttribute($key, $value);
+		return $this;
+	}
+
+
+	/**
+	 * Get value of the form element attribute.
+	 * @param  string  $key  Attribute name
+	 * @return mixed
+	 */
+	public function getAttribute($key)
+	{
+		return $this->getElementPrototype()->getAttribute($key);
+	}
+
+
+	/**
+	 * Signal - On successfull submit.
+	 * @param  static  $form  Form instance
+	 * @param  mixed   $data  Submited data
+	 * @return null
+	 */
+	protected function handleSuccess($form, $data)
+	{
+	}
 }
