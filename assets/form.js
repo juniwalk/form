d5992cfff73cb07e0396b94d6dabaafdd48e9e49
Martin Procházka <juniwalk@outlook.cz>
2023-08-23 19:19:10 +0200

    Do not store signals in history stack

--- assets/form.js
+++ assets/form.js
@@ -149,7 +149,7 @@ function initFormControls()
 		let formData = new FormData(this.form); formData.delete('_do');
 
 		displayRequestSpinner(this);
-		naja.makeRequest('POST', signalLink, formData);
+		naja.makeRequest('POST', signalLink, formData, {history: false});
 	});
 
 	$('[data-auto-submit]').off('change').on('change', function(e) {
