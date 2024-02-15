e7bd8544f9933d4996de85fa1feb3238ee596095
Martin Procházka <juniwalk@outlook.cz>
2024-02-15 15:26:58 +0100

    Drop support for select2

--- assets/form.js
+++ assets/form.js
@@ -110,39 +110,6 @@ function initFormControls()
 		let tomSelect = new TomSelect(el, options);
 	});
 
-	if ($().select2 !== undefined) $('select,input.select2').not('.tom-select,.custom-select,.flatpickr-monthDropdown-months').each(function() {
-		let formName = null;
-		let options = {
-			minimumResultsForSearch: 20,
-			templateSelection: select2Format,
-			templateResult: select2Format,
-		};
-
-		if (this.form && 'formName' in this.form.dataset) {
-			formName = this.form.dataset.formName + '-';
-		}
-
-		if (this.closest('.modal')) {
-			options.dropdownParent = this.parentNode.parentNode;
-		}
-
-		if (this.classList.contains('ajax') || this.dataset['ajax-Url'] !== undefined) {
-			options.minimumResultsForSearch = 0;
-			options.ajax = {delay: 250, cache: true, transport: (request, done, error) => {
-				let params = findPrefixedUrlParams(formName);
-				params[formName+'term'] = request.data.term || '';
-				params[formName+'page'] = request.data.page || 1;
-
-				naja.makeRequest(request.type, request.url, params, {history: false})
-					.then(done).catch(error);
-			}};
-		}
-
-		$(this).select2(options).on('select2:open', () => {
-			document.querySelector('.select2-container--open .select2-search__field').focus();
-		});
-	});
-
 	$('[data-signal]').off('click change').on('click change', function(e) {
 		if (!this.matches('BUTTON') && e.type == 'click') {
 			return;
@@ -268,24 +235,6 @@ function tomSelectFormat(type, data, escape, isMultiple)
 	}
 
 	return html.outerHTML;
-}
-
-
-function select2Format(state)
-{
-	let $option = $(state.element);
-	let $value = null;
-
-	if (($value = $option.data('content')) || ($value = state.content)) {
-		return $('<span>').html($value);
-	}
-
-	if (($value = $option.data('icon')) || ($value = state.icon)) {
-		let $color = ($option.data('color')) || (state.color);
-		return $('<span><i class="fa '+ $value +' fa-fw '+ $color +'"></i> '+ state.text +'</span>');
-	}
-
-	return state.text;
 }
 
 
