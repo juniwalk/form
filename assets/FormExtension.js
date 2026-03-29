84d35c66091a5f8a10416909c72c0c73708ba87a
juniwalk <juniwalk@outlook.cz>
2026-03-29 23:21:52 +0200

    Removed dispatching page.overlay event

--- assets/FormExtension.js
+++ assets/FormExtension.js
@@ -178,10 +178,6 @@ class FormExtension
 			element.classList.remove('is-invalid');
 		});
 
-		window.dispatchEvent(new CustomEvent('page.overlay.hide', {
-			detail: { target: element, isValid }
-		}));
-
 		return isValid;
 	}
 
